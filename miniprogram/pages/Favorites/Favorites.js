3272600ff5b2b27694a48a435520a8b65abdb308
xlcbingo1999 <43440113+xlcbingo1999@users.noreply.github.com>
2020-05-06 11:05:37 +0800

    create Mine 5pages UI 20200505

---
+++ miniprogram/pages/Favorites/Favorites.js
@@ -0,0 +1,91 @@
+// pages/Favorites/Favorites.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    favoritesItemList:[
+      {
+        'index':1,
+        'headImageUrl':'../../image/indexImage1.jpg',
+        'author':'linthings',
+        'favoritesTime':'2020-05-05',
+      },
+      {
+        'index':2,
+        'headImageUrl':'../../image/indexImage2.jpg',
+        'author':'linthings',
+        'favoritesTime':'2020-05-05',
+      },
+      {
+        'index':3,
+        'headImageUrl':'../../image/indexImage4.jpg',
+        'author':'linthings',
+        'favoritesTime':'2020-05-05',
+      },
+      {
+        'index':4,
+        'headImageUrl':'../../image/indexImage5.jpg',
+        'author':'linthin撒大苏打顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶gs',
+        'favoritesTime':'2020-05-05',
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})

ba0b38888967d8959f31b98034a05b91483f59ac
xlcbingo1999 <43440113+xlcbingo1999@users.noreply.github.com>
2020-05-07 00:36:05 +0800

    update(unfinished) RegisterForm UI 20200507

--- miniprogram/pages/RegisterForm/RegisterForm.js
+++ miniprogram/pages/RegisterForm/RegisterForm.js
@@ -5,7 +5,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showModalStatus: false,
+    name: '小明',
+    lawName: 'lintiang',
+    userId: '400000000000000000000000000',
+    type: '北京菜',
+    foodId: '40000000000000000000',
+    location: '广东省广州市海珠区新港东路1000号',
+    fixedPhone: '8801488',
+    mobPhone: '12345678901',
+    confirmId: 'ssssss',
+    introduction: '鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟婂晩鍟',
   },
 
   /**
@@ -62,5 +72,63 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  gotoNotice: function () {
+    wx.navigateTo({
+      url: '../notice/notice',
+    })
+  },
+
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function(currentStatu){
+    /* 动画部分 */
+    // 第1步：创建动画实例 
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长
+      timingFunction: "linear", //线性
+      delay: 0  //0则不延迟
+    });
+    
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+ 
+    // 第3步：执行第一组动画
+    animation.opacity(0).rotateX(-100).step();
+ 
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+    
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+      
+      //关闭
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+  
+    // 显示
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  },
 })

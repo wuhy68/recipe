e71876b42c1ed1fe600b8bb6c897e22eed5795e4
xlcbingo1999 <43440113+xlcbingo1999@users.noreply.github.com>
2020-05-11 19:44:23 +0800

    update registerForm 20200511

--- miniprogram/pages/RegisterForm/RegisterForm.js
+++ miniprogram/pages/RegisterForm/RegisterForm.js
@@ -5,10 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showModalStatus: false,
+    click: false, //是否显示弹窗内容
+    option: false, //显示弹窗或关闭弹窗的操作动画
     name: '小明',
     lawName: 'lintiang',
-    userId: '400000000000000000000000000',
+    userId: '458153193511543333',
     type: '北京菜',
     foodId: '40000000000000000000',
     location: '广东省广州市海珠区新港东路1000号',
@@ -78,57 +79,30 @@ Page({
       url: '../notice/notice',
     })
   },
-
-  powerDrawer: function (e) {
-    var currentStatu = e.currentTarget.dataset.statu;
-    this.util(currentStatu)
-  },
-  util: function(currentStatu){
-    /* 动画部分 */
-    // 第1步：创建动画实例 
-    var animation = wx.createAnimation({
-      duration: 200,  //动画时长
-      timingFunction: "linear", //线性
-      delay: 0  //0则不延迟
-    });
-    
-    // 第2步：这个动画实例赋给当前的动画实例
-    this.animation = animation;
- 
-    // 第3步：执行第一组动画
-    animation.opacity(0).rotateX(-100).step();
- 
-    // 第4步：导出动画对象赋给数据对象储存
-    this.setData({
-      animationData: animation.export()
-    })
-    
-    // 第5步：设置定时器到指定时候后，执行第二组动画
-    setTimeout(function () {
-      // 执行第二组动画
-      animation.opacity(1).rotateX(0).step();
-      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
-      this.setData({
-        animationData: animation
+  // 用户点击显示弹窗
+  clickPup: function() {
+    let _that = this;
+    if (!_that.data.click) {
+      _that.setData({
+        click: true,
       })
-      
-      //关闭
-      if (currentStatu == "close") {
-        this.setData(
-          {
-            showModalStatus: false
-          }
-        );
-      }
-    }.bind(this), 200)
-  
-    // 显示
-    if (currentStatu == "open") {
-      this.setData(
-        {
-          showModalStatus: true
-        }
-      );
+    }
+
+    if (_that.data.option) {
+      _that.setData({
+        option: false,
+      })
+
+      // 关闭显示弹窗动画的内容，不设置的话会出现：点击任何地方都会出现弹窗，就不是指定位置点击出现弹窗了
+      setTimeout(() => {
+        _that.setData({
+          click: false,
+        })
+      }, 500)
+    } else {
+      _that.setData({
+        option: true
+      })
     }
   },
 })
